46c87d631ff71904de0335d51b51640d19e2494e
AyushVV <ayushvaliparambil@gmail.com>
2024-04-29 13:44:51 +0530

    local files added

---
+++ src/main/java/com/mycompany/hotelbil/Hotelbil.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelbil;
+
+/**
+ *
+ * @author pc
+ */
+public class Hotelbil {
+
+    public static void main(String[] args) {
+       
+    }
+}
+
